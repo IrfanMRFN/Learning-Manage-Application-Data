769aa17ce65ffe8a23dcfafb55674c8f3d8bc12c
Muhammad Irfan <irfanmrfn28@gmail.com>
2026-02-18 12:53:11 +0700

    M4 commit

--- Data_M1/Data_M1/obj/Debug/net10.0/Data_M1.AssemblyInfo.cs
+++ Data_M1/Data_M1/obj/Debug/net10.0/Data_M1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data_M1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+947984e832cec748ad02e834e1e9403ab8021c10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2857ba5f65ae5728dddf3179dc04a17e2f3312")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data_M1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data_M1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
